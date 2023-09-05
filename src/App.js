7e7b4bbfbaec8240f965a512d2f2a64f2cca273b
shubhamshekhar256 <shubhamshekhar256@gmail.com>
2023-09-05 21:22:07 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+
+import Home from './components/Home';
+
+import DataProvider from './context/DataProvider';
+
+function App() {
+  return (
+        <DataProvider> 
+         <Home/>
+         </DataProvider>
+
+    
+   
+  );
+}
+
+export default App;
